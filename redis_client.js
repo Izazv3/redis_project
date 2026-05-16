235c70e4b3e4b41e9496b7740c2ac22cbd6c53fc
izazv3 <izazahamedpro2017@gmail.com>
2026-05-16 11:53:14 +0530

    sync

--- redis_client.js
+++ redis_client.js
@@ -1,11 +1,22 @@
 const redis = require("redis");
+require("dotenv").config();
 
-const client = redis.createClient();
-
-client.connect();
-
-client.on("connect", () => {
-  console.log("<<<<<< redis connected >>>>>>");
+const client = redis.createClient({
+  url: process.env.REDIS_URL,
 });
 
+client.on("error", (err) => {
+  console.log("❌ Redis Error:", err.message);
+});
+
+async function connectRedis() {
+  try {
+    await client.connect();
+    console.log("<<<<<< Redis connected >>>>>>");
+  } catch (err) {
+    console.log("❌ Redis connection failed:", err.message);
+  }
+}
+
+connectRedis();
 module.exports = client;
